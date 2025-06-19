4e60c8da59d94250a50a69e92b75cb3da79163e8
Brijesh1n1 <brijeshbhurtiya123aaa@gmail.com>
2025-06-19 11:28:34 +0530

    Initital commit

---
+++ core/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Machine, Quotation,Logo
+# Register your models here.
+
+admin.site.register(Machine)
+admin.site.register(Quotation)
+admin.site.register(Logo)
